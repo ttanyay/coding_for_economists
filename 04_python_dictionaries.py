e1d37260d90555af78480a0852db1f4a95a113cb
ttanyay <yakushina.tanya@gmail.com>
2024-01-18 13:56:42 +0000

    Added quadruple vanilla python

---
+++ 04_python_dictionaries.py
@@ -0,0 +1,15 @@
+# First look at dictionaries 
+
+myDict = {
+  "Dog": "Barks",
+  "Cat": "Meows",
+  "Whale": "Shreeks"
+}
+
+print(myDict)
+
+print(myDict.items())
+
+print(myDict["Dog"])
+
+print(len(myDict))
