6b1ebb2dd0b3b5efe7dd9cd6382441b7416c534a
ttanyay <yakushina.tanya@gmail.com>
2024-01-18 13:21:41 +0000

    Added two files with basic python code

---
+++ 02_basic_python.py
@@ -0,0 +1,57 @@
+# Basic arithmetic operations in Python
+
+# Add two numbers
+print(2+4)
+
+# Divide two numbers 
+print(10/5)
+
+# Careful
+print(type(10/5))
+
+print(2**3)
+
+x = 5
+y = x**2
+print(y)
+
+a = "Hello, how're you?"
+print(a*5)
+
+b = "I'm good, thank you."
+print(a + " " + b)
+
+# Subtraction
+# print(a - b)
+
+# Division
+# print(a/b)
+
+print(a[0])
+
+print(a[-1])
+
+print(a[0:5]) 
+
+print(len(a))
+
+print(f'Our string a has {len(a)} characters')
+
+#Alternative way
+print('Our string has {} characters'.format(len(a)))
+
+print(a[0:5])
+
+# Logical statements
+print(2 == 2)
+
+print(2 == 3)
+
+print(True == 1)
+
+print(a == b)
+
+print(2==2 and 3==3)
+print(2==2 and 3==4)
+
+print(2==2 or 3==4)
